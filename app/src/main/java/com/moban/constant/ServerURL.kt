e4cf1d2dce32e5a9e918410ed542b72b369d92ec
Nhan Neal-Nguyen <nguyenngocnhan90@gmail.com>
2024-03-07 09:59:29 +0700

    Correct api url to call

--- app/src/main/java/com/moban/constant/ServerURL.kt
+++ app/src/main/java/com/moban/constant/ServerURL.kt
@@ -6,7 +6,7 @@ package com.moban.constant
 class ServerURL {
     companion object {
         const val stagingURL: String = "https://test.ohio.net.vn/"
-        const val productionURL: String = "https://base.ohio.net.vn/"
+        const val productionURL: String = "https://admin.moban.vn/"
 
         const val linkmartURL: String = "https://linkmart.vn/wp-json/wc/v3/"
         const val secondaryProjectUrl: String = "https://thucap.ohio.net.vn/"
